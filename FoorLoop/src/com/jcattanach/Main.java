557f3b23d5d746d1af31e329c41160bd36cf0bf2
Jack Cattanach <jcattanach@clearcorrect.com>
2019-05-15 15:42:36 -0500

    For loop

--- FoorLoop/src/com/jcattanach/Main.java
+++ FoorLoop/src/com/jcattanach/Main.java
@@ -6,7 +6,12 @@ public class Main {
 	// write your code here
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
 
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
     }
+
+
 
     public static double calculateInterest(double amount, double interetRate){
         return amount * (interetRate / 100);
