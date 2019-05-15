2ac04cd8e34e9b057e8477e32d832f347c29f783
Jack Cattanach <jcattanach@clearcorrect.com>
2019-05-15 14:23:18 -0500

    methodOverloading

---
+++ MethodOverloading/src/com/jcattanach/Main.java
@@ -0,0 +1,26 @@
+package com.jcattanach;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int newScore =  calculateScore("Tim", 500);
+        System.out.println(newScore);
+
+        calculateScore(75);
+
+
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unamened player scored " + score + " points");
+        return score * 1000;
+    }
+
+
+}
