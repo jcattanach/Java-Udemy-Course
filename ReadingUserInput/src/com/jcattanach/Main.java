5ff452df8cf5795702cf9a51496252e8c5fc99da
Jack Cattanach <jcattanach@clearcorrect.com>
2019-05-16 13:17:23 -0500

    reading user input

--- ReadingUserInput/src/com/jcattanach/Main.java
+++ ReadingUserInput/src/com/jcattanach/Main.java
@@ -7,10 +7,18 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your birth year: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // handle enter key
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("your name is " +  name);
+        int age = 2019 - yearOfBirth;
+
+
+        System.out.println("your name is " +  name + ", and you are " + age + " years old.");
+
 
         scanner.close();
 
