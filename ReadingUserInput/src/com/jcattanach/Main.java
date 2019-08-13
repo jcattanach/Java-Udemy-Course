c3b19f29eaa77df4fb56d97819d31f5282bddd60
Jack Cattanach <jcattanach@clearcorrect.com>
2019-08-13 14:14:26 -0500

    composition

--- ReadingUserInput/src/com/jcattanach/Main.java
+++ ReadingUserInput/src/com/jcattanach/Main.java
@@ -8,14 +8,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your birth year: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2019 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2019 - yearOfBirth;
+
+            if(age >= 0 && age <= 120){
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+
+            } else {
+                System.out.println("Invalid year of birth...");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth...");
+        }
 
         scanner.close();
 
