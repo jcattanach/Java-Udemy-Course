5bc6fb37471209f04d156dd529ccc12a270f29e9
Jack Cattanach <jcattanach@clearcorrect.com>
2019-08-27 15:39:15 -0500

    array challenges

--- ReverseArrayChallenge/src/com/jcattanach/Main.java
+++ ReverseArrayChallenge/src/com/jcattanach/Main.java
@@ -1,12 +1,30 @@
 package com.jcattanach;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = {1,5,3,7,11,15};
+
+        System.out.println(Arrays.toString(array));
+
+        reverseArray(array);
+
+        System.out.println(Arrays.toString(array));
+
     }
 
     private static void reverseArray(int[] array){
-        array = 
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for(int i = 0; i < halfLength; i++){
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+
     }
 }
